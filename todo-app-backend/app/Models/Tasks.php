ffd07a2d404551ebecc6b333acfa81c35aed0865
Joe-Klenam <klenamj3@gmail.com>
2023-06-07 23:39:03 +0000

    added title column to the tasks table

--- todo-app-backend/app/Models/Tasks.php
+++ todo-app-backend/app/Models/Tasks.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tasks extends Model
 {
-    use HasFactory;
+        use HasFactory;
 }
--- todo-app-backend/database/migrations/2023_06_07_102116_create_tasks_table.php
+++ todo-app-backend/database/migrations/2023_06_07_102116_create_tasks_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger("user_id");
             $table->enum('status',['completed','active']);
-            $table->integer('rank');
+            $table->integer('priority');
+            $table->text("title");
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
