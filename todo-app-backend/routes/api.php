6285b653061d1247523cc84caa5ea8ebed2592d6
Joe-Klenam <klenamj3@gmail.com>
2023-06-08 12:47:40 +0000

    Created a new endpoint to fetch tasks

--- todo-app-backend/routes/api.php
+++ todo-app-backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TasksController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/login', [AuthController::class,'login']);
 Route::post('/signup',[AuthController::class,'signup']);
+Route::post('/logout',[AuthController::class,'logout']);
+Route::get('/tasks', [TasksController::class,'tasks']);
 
