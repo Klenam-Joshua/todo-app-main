a874561bb4590a3ecdf6f202904a46193da904ba
Joe-Klenam <klenamj3@gmail.com>
2023-06-08 11:10:01 +0000

    created Task controller class at the backend

---
+++ todo-app-backend/app/Http/Controllers/TasksController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    public function tasks(){
+
+    }
+
+    public function destroy(){
+
+    }
+
+    public function  update(){
+        
+    }
+
+
+}
