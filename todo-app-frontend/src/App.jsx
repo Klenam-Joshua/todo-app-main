5045f7dfe31f8c414f1499c557eddecdfeb24c80
Joe-Klenam <klenamj3@gmail.com>
2023-06-09 08:20:45 +0000

    created route,  /todos

--- todo-app-frontend/src/App.jsx
+++ todo-app-frontend/src/App.jsx
@@ -1,11 +1,24 @@
+import Todo from './Components/Todo/Todo';
+import Signup from './Components/Signup/Signup';
+import Login from './Components/Login/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react'
+import { useState } from 'react';
+
 
 function App() {
-  return (
-    <div>
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
 
-    </div>
-  )
+  return (
+
+    <Router>
+      <Routes>
+        <Route exact path='/todos' element={Todo} />
+      </Routes>
+    </Router>
+
+  );
 }
 
 export default App
