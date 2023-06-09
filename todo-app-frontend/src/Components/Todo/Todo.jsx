082fa2881b7194be900e982aedbdd47598d57a62
Joe-Klenam <klenamj3@gmail.com>
2023-06-09 09:55:13 +0000

    applied error handling the fetch

--- todo-app-frontend/src/Components/Todo/Todo.jsx
+++ todo-app-frontend/src/Components/Todo/Todo.jsx
@@ -7,38 +7,67 @@ import {
     iconMoon,
     iconSun
 }
-    from '../Images';
+    from '../../Components/Images';
+
+import Login from '../Login/Login';
 
 
 
+const Todo = ({ token, user }) => {
+    if (!token) {
+        navigate('/login');
+    }
 
-
-const Todo = () => {
+    const navigate = useNavigate();
     const [data, setData] = useState(null);
     const [filteredData, setFilteredData] = useState(null);
+    const [notFound, setNotFound] = useState(false);
+    const [responseError, setResponseError] = useState(false);
+
+
+
     useEffect(() => {
         async function fetchTodos() {
             const url = 'http://127.0.0.1:8001/api/';
             const header = {
                 'content-Type': 'application/json',
+                'token': `Bearer:${token}`
             }
+            try {
 
-            let response = await fetch(`${url}/todos`, { header });
-            let data = await response.json();
-            setData(data);
+                let response = await fetch(`${url}/todos`, { header });
+                if (response.status === 401) {
+                    navigate('/login');
+                }
+                else if (response.status === 404) {
+                    setNotFound(true);
+                }
+
+                if (!response.ok) {
+                    setResponseError(true);
+                }
+
+                else if (response.status === 200) {
+                    let data = await response.json();
+                    setData(data);
+                    setFilteredData(data);
+                }
+            }
+            catch (error) {
+                console.log(error);
+            }
         }
+
     }, [])
 
-    // if (!loginToken) {
 
-    // }
     return (
         <main className="container">
             <header className='row' id='header'>
 
             </header>
             <section className="row">
-                <img src={imgBgDesktopDark} alt="" />
+
             </section>
         </main>
     )
