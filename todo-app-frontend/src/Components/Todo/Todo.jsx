104642f26634a6e9423e1447ec888e7b5d80288f
Joe-Klenam <klenamj3@gmail.com>
2023-06-09 06:36:45 +0000

    modified Todo.jsx file to fetch data from the backend

--- todo-app-frontend/src/Components/Todo/Todo.jsx
+++ todo-app-frontend/src/Components/Todo/Todo.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+    imgBgDesktopDark, imgBgDesktopLight, imgBgMobileDark, imgBgMobileLight, imgFavicon, iconCheck,
+    iconCross,
+    iconMoon,
+    iconSun
+}
+    from '../Images';
+
+
+
+
 
 const Todo = () => {
-    return (
-        <div>
+    const [data, setData] = useState(null);
+    const [filteredData, setFilteredData] = useState(null);
+    useEffect(() => {
+        async function fetchTodos() {
+            const url = 'http://127.0.0.1:8001/api/';
+            const header = {
+                'content-Type': 'application/json',
+            }
 
-        </div>
+            let response = await fetch(`${url}/todos`, { header });
+            let data = await response.json();
+            setData(data);
+        }
+    }, [])
+
+    // if (!loginToken) {
+
+    // }
+    return (
+        <main className="container">
+            <header className='row' id='header'>
+
+            </header>
+            <section className="row">
+                <img src={imgBgDesktopDark} alt="" />
+            </section>
+        </main>
     )
 }
 
