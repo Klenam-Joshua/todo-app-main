eccebab5565a543b15b78c11ccc42ad307a8a90b
Joe-Klenam <klenamj3@gmail.com>
2023-06-11 05:01:56 +0000

    created a signup form

--- todo-app-frontend/src/Components/Signup/Signup.jsx
+++ todo-app-frontend/src/Components/Signup/Signup.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
+import "./Signup.css";
 
 const Signup = () => {
     return (
-        <div>
+        <div className='wrapper'>
+            <h2>sign up</h2>
+            <div className="form_container  w-5">
+                <form >
+                    <div>
+                        <label htmlFor="username">
+                            username
+                        </label>
+                        <input type="text" id="username" name="username" />
+                    </div>
+                    <div>
+                        <label htmlFor="email">
+                            email
+                        </label>
+                        <input type="email" id="email" name="useremail" />
+                    </div>
+                    <div>
+                        <label htmlFor="password">
+                            password
+                        </label>
+                        <input type="password" id="password" name="userpassword" />
+                    </div>
+                    <div>
+                        <label htmlFor="confirm_password">
+                            confirm password
+                        </label>
+                        <input type="password" id="confirm_password" name="confirm_password" />
+                    </div>
+                </form>
+            </div>
+            <p>
+                <span>
+                    already have an account ?
+                </span>
+                <a href='/login'>
+                    login here !
+                </a>
+            </p>
 
         </div>
-    )
+    );
 }
 
 export default Signup
