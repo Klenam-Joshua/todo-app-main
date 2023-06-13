d5a41b5a6417bf34ff956a1e53043665ed08799b
Joe-Klenam <klenamj3@gmail.com>
2023-06-13 09:51:44 +0000

    modified the logic for retrieving tasks from the backend

--- todo-app-backend/app/Http/Controllers/TasksController.php
+++ todo-app-backend/app/Http/Controllers/TasksController.php
@@ -5,35 +5,56 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use  App\Models\Tasks;
 use App\Models\User;
+use App\Http\Requests\TasksRequest;
 use Illuminate\Support\Facades\Auth;
 
 class TasksController extends Controller
 {
+  protected $headers =['Access-Control-Allow-Origin'=> '*',
+  'Access-Control-Allow-Methods'=>'POST'];
     public function tasks(Request $request){
+   
      $userId = $request->header("userId");
-      $user =  User::where('user_id',$userId);
-      if(!$user){
-          return response()->json("not found",404);
-      }
-     $todos =  Tasks::where("user_id",$userId)->orderBy('priority','asc');
+      $user =  User::find($userId);
+      
 
-      return response()->json($todos);
+       $data = Tasks::where('user_id',$userId)->orderBy('priority','asc')->get();
+       return response()->json($data,200,$this->headers);
+       
+       // return response()->json($todos,200,$headers);
              
     }
 
-    public function destroy(Request $request){
-                
-            $totalTodoCount = todos::where('status','active')->count();
+    public function createTask(TasksRequest $request ){ 
+             $data = $request->validated();
+             $id = $request->header('userId');
+             $data2 = [
+                  'user_id' => (int)$id,
+                  'status' => $data['status'],
+                  'priority' => $data['priority'],
+                  'title'=>$data['title']
+             ];
+           
+            if(!Tasks::create($data2)){
+                return response()->json("error creating task");
 
-            $allTodo = todos::where('status','active')->get();
-            $lessData = todos::where("id", '<' ,$id)->where("status","active")->count();
+            }
+            return response()->json("task created successfully");
+    }
+
+    public function destroy(Request $request,$id){
+          $userId = $request->header('userId');
+            $totalTodoCount = Tasks::where('user_id',$userId)->count();
+
+            $allTodo = Tasks::where('user_id',$userId)->get();
+            $lessData = Tasks::where("id", '<' ,$id)->where("user_id",$userId)->count();
            
             if(!$lessData){
                  $lessData = 0;
               }
 
 
-              $tupleTodelete= todos::find($id);
+              $tupleTodelete= Tasks::find($id);
                if( $tupleTodelete){
                  $tupleTodelete->delete();
 
@@ -42,8 +63,8 @@ class TasksController extends Controller
               for($i = $lessData +1; $i < $totalTodoCount ; $i++ )
               {
                    $id = $allTodo[$i]->id;
-                   $tuple = todos::find($id) ;
-                   $tuple->todoRank = $i;
+                   $tuple = Tasks::find($id) ;
+                   $tuple->priority = $i;
                    $tuple->save();
                      
 
