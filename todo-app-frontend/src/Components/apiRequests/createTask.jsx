1fdbf269825bc22ae6f30a6d80ab0a0cea9e1ec6
Joe-Klenam <klenamj3@gmail.com>
2023-06-14 17:26:29 +0000

    create a function to submit add new tasks

---
+++ todo-app-frontend/src/Components/apiRequests/createTask.jsx
@@ -0,0 +1,44 @@
+
+
+const createTask = (data, token, userId) => {
+    const createtask = async function () {
+
+        const url = 'http://127.0.0.1:8000/api/';
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                'content-Type': 'application/json',
+                'token': ` Bearer ${token}`,
+                'userId': userId
+
+            },
+            body: JSON.stringify(data)
+        }
+        try {
+
+            let response = await fetch(`${url}createtask`, requestOptions);
+
+            if (response.status === 401) {
+                navigate('/login');
+            }
+
+            if (!response.ok) {
+                responseError = true;
+            }
+
+            else if (response.status === 200) {
+                responseText = await response.text();
+
+
+            }
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    createtask()
+
+}
+
+export default createTask
