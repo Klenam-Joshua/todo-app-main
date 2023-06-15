027932bbf3893dccc8e360ee88cb35eb5045480c
Joe-Klenam <klenamj3@gmail.com>
2023-06-15 01:21:21 +0000

    added contact attribute to the users table

--- todo-app-backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ todo-app-backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->unsignedBigInteger("contact");
             $table->timestamps();
         });
     }
