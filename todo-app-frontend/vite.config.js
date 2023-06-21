bd7444be019cbe83f67b745b8e29705c42c05992
Joe-Klenam <klenamj3@gmail.com>
2023-06-21 09:54:14 +0000

    added a line to load the .env file

--- todo-app-frontend/vite.config.js
+++ todo-app-frontend/vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
 
+dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 
 
 })
+
+
+
+
+ // Load environment variables from .env file
+
+
